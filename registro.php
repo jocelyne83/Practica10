0ee2801957b2a69dff51727938e44e1c8774c80b
jocelyne83 <145421537+jocelyne83@users.noreply.github.com>
2023-10-29 19:27:32 -0600

    Update registro.php

--- registro.php
+++ registro.php
@@ -18,22 +18,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nombre = $_POST["nombre"];
     $apellido = $_POST["apellido"];
     $email = $_POST["email"];
-    $password = $_POST["password"];
-
-    // Hash de la contraseña (debes mejorar esto con una técnica de hashing segura)
-    $hashed_password = md5($password);
+    $password = $_POST["password"]; // Aquí ya no utilizamos password_hash
 
     // Consulta SQL para insertar el nuevo usuario en la base de datos
-    $consulta = "INSERT INTO usuarios (nombre, apellido, email, contraseña) VALUES ('$nombre', '$apellido', '$email', '$hashed_password')";
+    $consulta = "INSERT INTO usuarios (nombre, apellido, email, password) VALUES ('$nombre', '$apellido', '$email', '$password')";
 
     if ($cone->query($consulta) === TRUE) {
-        echo "Registro exitoso. Ahora puedes <a href='login.html'>iniciar sesión</a>.";
+        echo "Registro exitoso.";
+        header("Location: login.html");
     } else {
         echo "Error en el registro: " . $cone->error;
     }
 }
 
+
 // Cerrar la conexión a la base de datos
 $cone->close();
 ?>
-
